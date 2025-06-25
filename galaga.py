91513b62382f016824564173a5ab5765cbf97384
Kush Bajaria <123843718+bajariakush@users.noreply.github.com>
2025-06-25 11:54:34 -0700

    Add files via upload

---
+++ galaga.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''main program for galaga game'''
+import sys
+import pygame
+from videogame import game
+
+
+def main():
+    '''main function for galaga game'''
+    print('hello world.')
+
+if __name__ == '__main__':
+    sys.exit(game.VideoGame().run())
